4a5a021cfd4ee24fb14c73739ab07fa1b9780671
Navidreza Haghighi <navidhaghighi2013@gmail.com>
2026-05-29 21:51:27 +0330

    newindex.js

--- newindex.js
+++ newindex.js
@@ -1,5 +1,4 @@
 const display = document.getElementById("displaynum");
-<<<<<<< HEAD
 let buttons = document.getElementById('buttons');
 
 function addnum(num) {
@@ -7,42 +6,24 @@ function addnum(num) {
     display.value += num;
 }
 
-function addnum(input){
-     playSound();
-=======
-
-function addnum(input){
->>>>>>> 88f1600fceea88db551c621df43c882bc2689694
-    display.value += input;
-}
-
-// THIS function should perform the math
 function calculate(){
-<<<<<<< HEAD
-     playSound();
-=======
->>>>>>> 88f1600fceea88db551c621df43c882bc2689694
+    playSound();
     try {
         display.value = eval(display.value);
     } catch(error) {
         display.value = "Error";
     }
 }
-<<<<<<< HEAD
+
 function clearcalc(){
-     playSound();
+    playSound();
     display.value = "";
 }
 
 function playSound() {
     const sound = document.getElementById('button-sound');
-    sound.currentTime = 0;
-    sound.play();
+    if (sound) {
+        sound.currentTime = 0;
+        sound.play();
+    }
 }
-=======
-
-// THIS function should just clear the screen
-function clearcalc(){
-    display.value = "";
-}
->>>>>>> 88f1600fceea88db551c621df43c882bc2689694
